cbb5d7e19a9f09938730d431a3deb0341b6c2e8b
anthony <contact.ahengy@gmail.com>
2018-05-10 15:20:16 +0200

    added json register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use App\Form\UserType;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegistrationController extends Controller
 {
@@ -44,4 +47,54 @@ class RegistrationController extends Controller
             array('form' => $form->createView())
         );
     }
+
+    /**
+     *  Register the user
+     *
+     * @Route("/jsonregister", name="user_json_registration")
+     *
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function jsonRegister(Request $request, UserPasswordEncoderInterface $passwordEncoder, ValidatorInterface $validator)
+    {
+        if ($request->isMethod('POST')) {
+            $user = new User();
+            // getting json post parameters
+            if ($request->getContent()){
+                $params = json_decode($request->getContent(), true);
+
+                $username =  $params['login'];
+                $password =  $params['password'];
+                $email = $params['email'];
+            }
+
+            if(!is_null($username) && !is_null($password) && !is_null($email)){
+                $user->setEmail($email);
+                $user->setPlainPassword($password);
+                $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+                $user->setPassword($password);
+                $user->setUsername($username);
+
+                $errors = $validator->validate($user);
+                if (count ($errors) > 0 ){
+                    $errorsString = (string) $errors;
+                    return new JsonResponse($errorsString);
+                }
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                return new JsonResponse(true);
+            }
+
+           return new JsonResponse(false);
+        }
+
+        throw new \Exception('Wrong parameters');
+    }
+
 }
